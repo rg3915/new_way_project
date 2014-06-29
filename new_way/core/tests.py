f6a945f6027a4d1bf36c56e03eba39068af847f3
rg3915 <rg3915@yahoo.com.br>
2014-06-29 19:35:44 -0300

    Fisrt test

--- new_way/core/tests.py
+++ new_way/core/tests.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+
+    def test_get(self):
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
