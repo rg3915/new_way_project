b1f2ef0e825b99b5bcb63cd77ee99ffe28cc5e8d
rg3915 <rg3915@yahoo.com.br>
2014-07-01 22:20:05 -0300

    HomeTest completo

--- new_way/core/tests.py
+++ new_way/core/tests.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+"""
+Usando um assert por método de test.
+Teste de Request e Response.
+"""
 
 
 class HomeTest(TestCase):
 
+    def setUp(self):
+        """
+        Nome especial de método do Python, roda sempre antes de cada teste.
+        Armazena a resposta da requisição.
+        """
+        self.resp = self.client.get('/')
+
     def test_get(self):
-        response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'index.html')
+        """
+        GET /retorna o status code 200, sucesso.
+        """
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        """
+        Página usada no template index.html
+        """
+        self.assertTemplateUsed(self.resp, 'index.html')
