18e8ddd048afac25683b4ad41e16b0d450ccee4a
rg3915 <rg3915@yahoo.com.br>
2014-07-01 22:58:55 -0300

    Teste de rota

--- new_way/inscricoes/tests.py
+++ new_way/inscricoes/tests.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 
-# Create your tests here.
+
+class InscricaoTest(TestCase):
+
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
+    def test_get(self):
+        'GET /inscricao/ retorna status code 200.'
+        self.assertEqual(200, self.resp.status_code)
