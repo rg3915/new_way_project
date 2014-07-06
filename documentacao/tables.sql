997564e71d2a24634fe3f0b74a96c0892a352384
rg3915 <rg3915@yahoo.com.br>
2014-07-05 23:43:03 -0300

    tables.sql

---
+++ documentacao/tables.sql
@@ -0,0 +1,18 @@
+# tables.sql
+
+CREATE TABLE "subscriptions_subscription" (
+	"id" integer NOT NULL PRIMARY KEY,
+	"firstname" varchar(100) NOT NULL,
+	"lastname" varchar(100) NOT NULL,
+	"cpf" varchar(11) NOT NULL UNIQUE,
+	"date_of_birth" datetime NOT NULL,
+	"email" varchar(75) NOT NULL,
+	"phone" varchar(20) NOT NULL,
+	"cell" varchar(20) NOT NULL,
+	"address" varchar(150) NOT NULL,
+	"complement" varchar(100),
+	"district" varchar(100) NOT NULL,
+	"city" varchar(100) NOT NULL,
+	"uf" varchar(100) NOT NULL,
+	"cep" varchar(8) NOT NULL,
+	"created_at" datetime NOT NULL
---
+++ new_way/subscriptions/tests/test_models.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from new_way.subscriptions.models import Subscription
