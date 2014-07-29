f8a80328fe3f7f80d9fd1ba48cace7dd780a2fc5
rg3915 <rg3915@yahoo.com.br>
2014-07-29 00:07:24 -0300

    CEP esta funcionando

--- new_way/subscriptions/selenium/get_cep.py
+++ new_way/subscriptions/selenium/get_cep.py
@@ -3,22 +3,10 @@ from cep import Correios
 from generate_random_values import *
 from list_cep import get_cep
 
-# Não funciona
-# n_cep = '04696000'
-# n_cep = "%08i" % generate_cep()
-# c = Correios()
-# r = None
-
-# while r is None:
-#     try:
-#         r = c.consulta(n_cep, primeiro=True)
-#     except Exception, e:
-#         raise e
-
 # Escolhe um CEP válido em list_cep
 cep = get_cep()
 c = Correios()
 r = c.consulta(cep, primeiro=True)
-print -r['CEP']
+print r['CEP']
 print r['Logradouro']
 print r
