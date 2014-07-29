cd3d897d3550e7a870e40faa5d18f46bea9aa311
rg3915 <rg3915@yahoo.com.br>
2014-07-29 00:19:40 -0300

    todo

--- new_way/core/urls.py
+++ new_way/core/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     'new_way.core.views',
     url(r'^$', 'home', name='home'),
+    # todo
 )
--- new_way/core/views.py
+++ new_way/core/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+# todo
