6dd8a3d7708264e4a77471c98ad77215feea57d7
Regis da Silva <regis.santos.100@gmail.com>
2015-06-05 02:14:15 -0300

    OrderedForm

--- new_way/core/forms.py
+++ new_way/core/forms.py
@@ -1,9 +1,19 @@
 from django import forms
-from .models import Customer
+from .models import Customer, Ordered
+from .lists import gender_list, uf_list
 
 
 class CustomerForm(forms.ModelForm):
+    gender = forms.ChoiceField(
+        label='Sexo', choices=gender_list, initial='M', widget=forms.RadioSelect)
 
     class Meta:
         model = Customer
+        fields = '__all__'
+
+
+class OrderedForm(forms.ModelForm):
+
+    class Meta:
+        model = Ordered
         fields = '__all__'
